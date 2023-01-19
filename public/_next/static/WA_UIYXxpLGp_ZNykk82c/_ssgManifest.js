23075b5b53de3d9a77e17622d249323c3c041c5c
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-19 16:48:24 +0200

    Create _ssgManifest.js

---
+++ public/_next/static/WA_UIYXxpLGp_ZNykk82c/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fattachments","\u002Fauthentication","\u002Fcontacts","\u002Fconversations","\u002Ferrors","\u002Fgroups","\u002Fmessages","\u002Fpagination","\u002Fquickstart","\u002Fsdks","\u002Fwebhooks"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
