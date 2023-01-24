c45ed5e0502b07912b60b36aebbbe1aed8a01565
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-24 16:43:40 +0200

    Create typewriter-custom.js

---
+++ public/assets/js/typewriter-custom.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/* ========= Typewriter effect =========== */
+/* Ref: https://safi.me.uk/typewriterjs/ */
+const instance = new Typewriter('#typewriter', {
+  strings: ['JS Library', 'jQuery Plugin', 'Browser Extension', 'CMS Plugin', 'Mobile App', 'Open Source Project'],
+  autoStart: true,
+  loop: true,
+});
+
+
