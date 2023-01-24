e3e12d45fc50d6e4adbafcba8dea990f3132904c
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-24 17:25:17 +0200

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,8 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function PaginaInicial()
+    {
+        return view('docs.v01Api');
+    }
 }
