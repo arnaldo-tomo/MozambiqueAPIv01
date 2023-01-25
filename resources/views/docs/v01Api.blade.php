98fdbc83a228d775f89b26052121aed17512b05b
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-26 00:39:28 +0200

    Update v01Api.blade.php

--- resources/views/docs/v01Api.blade.php
+++ resources/views/docs/v01Api.blade.php
@@ -179,7 +179,8 @@
     <span class="hljs-attr">"Admitido nas Nações Unidas"</span>: <span class="hljs-string">"6 de Setembro de 1975"</span>,
     <span class="hljs-attr">"Guerra Civil Moçambicana"</span>: <span class="hljs-string">"1977–1992"</span>,
     <span class="hljs-attr">"Constituição atual"</span>: <span class="hljs-string">"21 de Dezembro de 2004"</span>,
-    <span class="hljs-attr">"Área Total"</span>: <span class="hljs-string">"21 de Dezembro de 2004"</span>,
+    <span class="hljs-attr">"Área Total"</span>: <span class="hljs-string">"801.590 km2 (309.500 sq mi) (35º)"</span>,
+    <span class="hljs-attr">"Área Total"</span>: <span class="hljs-string">"801.590 km2 (309.500 sq mi) (35º)"</span>,
   }
 ]
 
