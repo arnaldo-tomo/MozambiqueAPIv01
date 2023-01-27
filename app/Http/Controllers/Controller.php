3d890c10bb9944b5757085b73193c9d731c1e8a4
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-27 16:58:26 +0200

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Hash;
 
 class Controller extends BaseController
 {
@@ -13,5 +15,16 @@ class Controller extends BaseController
     public function PaginaInicial()
     {
         return view('docs.v01Api');
+    }
+
+    public function save()
+    {
+        //para criar admin
+        $usuario = new User;
+        $usuario->name = "Arnaldo Jose Tomo";
+        $usuario->email = "admin@gmail.com";
+        $usuario->password = Hash::make('admin');
+        $usuario->save();
+        return "Craido Com Exito! ";
     }
 }
