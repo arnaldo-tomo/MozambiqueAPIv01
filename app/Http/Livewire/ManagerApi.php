ac24a64d5abde5ba8464e14bccca1c9e67c03063
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-27 16:58:40 +0200

    Create ManagerApi.php

---
+++ app/Http/Livewire/ManagerApi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ManagerApi extends Component
+{
+    public function render()
+    {
+        return view('livewire.manager-api');
+    }
+}
