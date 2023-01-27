2a24e215621aabcf199bf4172647eb5db368733d
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-27 17:01:17 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Controller;
 use App\Http\Livewire\Login;
+use App\Http\Livewire\ManagerApi;
 use Illuminate\Support\Facades\Route;
 
 route::get('/', [Controller::class, 'PaginaInicial']);
 route::get('cpainel', Login::class);
+route::get('managerapi', ManagerApi::class)->name('manegerapi');
