f7a4825db0141969410589d175a7f5b1a5b9a791
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-27 13:54:16 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use App\Http\Livewire\Login;
 use Illuminate\Support\Facades\Route;
 
 route::get('/', [Controller::class, 'PaginaInicial']);
+route::get('cpainel', [Login::class, 'render']);
