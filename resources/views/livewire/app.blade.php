bf101d6704edf8273bb52278114204427510a428
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-28 15:09:53 +0200

    Update app.blade.php

--- resources/views/livewire/app.blade.php
+++ resources/views/livewire/app.blade.php
@@ -3,6 +3,8 @@
     <html lang="en">
     <head>
         <link rel="shortcut icon" href="assets/icon.png">
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+
         <title>{{ config('app.name') }}</title>
         @livewireStyles
     </head>
@@ -10,6 +12,8 @@
     <body>
         {{ $slot }}
         @livewireScripts
+            <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+
     </body>
     </html>
 </div>
