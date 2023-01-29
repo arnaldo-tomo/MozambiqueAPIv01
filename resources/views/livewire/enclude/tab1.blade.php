641aa9edf1fbc7ff0f3f4f7b61ec7bbcbdb75818
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-29 14:12:30 +0200

    Create tab1.blade.php

---
+++ resources/views/livewire/enclude/tab1.blade.php
@@ -0,0 +1,285 @@
+<div class="row my-4">
+    <div class="col">
+        <div class="accordion" id="collapsibleSection">
+            <!-- Delivery Address -->
+            <div class="card accordion-item">
+                <h2 class="accordion-header" id="headingDeliveryAddress">
+                    <button class="accordion-button" type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseDeliveryAddress"
+                        aria-expanded="true"
+                        aria-controls="collapseDeliveryAddress">Geral
+                    </button>
+                </h2>
+                <div id="collapseDeliveryAddress"
+                    class="accordion-collapse collapse show"
+                    aria-labelledby="headingDeliveryAddress"
+                    data-bs-parent="#collapsibleSection">
+                    <div class="accordion-body">
+                        <form>
+                            <div class="accordion-body">
+                                <div class="row">
+                                    <div class="col-12 col-md-12 col-xl-12">
+                                                                             
+                                        <div class="row">
+                                            <div class="col-12 col-md-2">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Moeda</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-2">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Código ISO 3166</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Capital</label>
+                                                    <input type="text" class="form-control" placeholder="<aputo" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Cidade Mas Populosa</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Internet TLD</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Código de chamada</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Lado de condução</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Formato de data</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Formato de data</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Formato de data</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Formato de data</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Formato de data</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt-1">
+                                    <button type="submit"
+                                        class="btn btn-primary me-sm-3 me-1">Submit</button>
+                                    <button type="reset"
+                                        class="btn btn-label-secondary">Cancel</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- Delivery Options -->
+            <div class="card accordion-item">
+                <h2 class="accordion-header" id="headingDeliveryOptions">
+                    <button class="accordion-button collapsed"
+                        type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseDeliveryOptions"
+                        aria-expanded="false"
+                        aria-controls="collapseDeliveryOptions"> Politica
+                    </button>
+                </h2>
+                <div id="collapseDeliveryOptions"
+                    class="accordion-collapse collapse"
+                    aria-labelledby="headingDeliveryOptions"
+                    data-bs-parent="#collapsibleSection">
+                    <div class="accordion-body">
+                        <form>
+                            <div class="accordion-body">
+                                <div class="row">
+                                    <div class="col-12 col-md-8 col-xl-6">
+                                        <div class="mb-3">
+                                            <label class="form-label w-100"
+                                                for="creditCardMask">Card
+                                                Number</label>
+                                            <div
+                                                class="input-group input-group-merge">
+                                                <input type="text"class="form-control credit-card-mask"
+                                                    placeholder="1356 3215 6548 7898"
+                                                    aria-describedby="creditCardMask2" />
+                                                <span
+                                                    class="input-group-text cursor-pointer p-1"
+                                                    id="creditCardMask2"><span class="card-type"></span></span>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-12 col-md-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Name</label>
+                                                    <input type="text" class="form-control" placeholder="John Doe" />
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="collapsible-payment-expiry-date">Exp. Date</label>
+                                                    <input type="text" id="collapsible-payment-expiry-date" class="form-control expiry-date-mask" placeholder="MM/YY" />
+                                                </div>
+                                            </div>
+                                            <div class="col-6 col-md-3">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="collapsible-payment-cvv">CVV Code</label>
+                                                    <div
+                                                        class="input-group input-group-merge">
+                                                        <input type="text"
+                                                            id="collapsible-payment-cvv"
+                                                            class="form-control cvv-code-mask"
+                                                            maxlength="3"
+                                                            placeholder="654" />
+                                                        <span
+                                                            class="input-group-text cursor-pointer"
+                                                            id="collapsible-payment-cvv2"><i
+                                                                class="bx bx-help-circle text-muted"
+                                                                data-bs-toggle="tooltip"
+                                                                data-bs-placement="top"
+                                                                title="Card Verification Value"></i></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt-1">
+                                    <button type="submit"
+                                        class="btn btn-primary me-sm-3 me-1">Submit</button>
+                                    <button type="reset"
+                                        class="btn btn-label-secondary">Cancel</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- Payment Method -->
+            <div class="card accordion-item">
+                <h2 class="accordion-header" id="headingPaymentMethod">
+                    <button class="accordion-button collapsed"
+                        type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapsePaymentMethod"
+                        aria-expanded="false"
+                        aria-controls="collapsePaymentMethod"> Economico
+                    </button>
+                </h2>
+                <div id="collapsePaymentMethod"
+                    class="accordion-collapse collapse"
+                    aria-labelledby="headingPaymentMethod"
+                    data-bs-parent="#collapsibleSection">
+                    <form>
+                        <div class="accordion-body">
+                            <div class="row">
+                                <div class="col-12 col-md-8 col-xl-6">
+                                    <div class="mb-3">
+                                        <label class="form-label w-100"
+                                            for="creditCardMask">Card
+                                            Number</label>
+                                        <div
+                                            class="input-group input-group-merge">
+                                            <input type="text"
+                                                id="creditCardMask"
+                                                name="creditCardMask"
+                                                class="form-control credit-card-mask"
+                                                placeholder="1356 3215 6548 7898"
+                                                aria-describedby="creditCardMask2" />
+                                            <span
+                                                class="input-group-text cursor-pointer p-1"
+                                                id="creditCardMask2"><span
+                                                    class="card-type"></span></span>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-12 col-md-6">
+                                            <div class="mb-3">
+                                                <label class="form-label"
+                                                    for="collapsible-payment-name">Name</label>
+                                                <input type="text"
+                                                    class="form-control"
+                                                    placeholder="John Doe" />
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div class="mb-3">
+                                                <label class="form-label"
+                                                    for="collapsible-payment-expiry-date">Exp.
+                                                    Date</label>
+                                                <input type="text"
+                                                    id="collapsible-payment-expiry-date"
+                                                    class="form-control expiry-date-mask"
+                                                    placeholder="MM/YY" />
+                                            </div>
+                                        </div>
+                                        <div class="col-6 col-md-3">
+                                            <div class="mb-3">
+                                                <label class="form-label"
+                                                    for="collapsible-payment-cvv">CVV
+                                                    Code</label>
+                                                <div
+                                                    class="input-group input-group-merge">
+                                                    <input type="text"
+                                                        id="collapsible-payment-cvv"
+                                                        class="form-control cvv-code-mask"
+                                                        maxlength="3"
+                                                        placeholder="654" />
+                                                    <span
+                                                        class="input-group-text cursor-pointer"
+                                                        id="collapsible-payment-cvv2"><i
+                                                            class="bx bx-help-circle text-muted"
+                                                            data-bs-toggle="tooltip"
+                                                            data-bs-placement="top"
+                                                            title="Card Verification Value"></i></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-1">
+                                <button type="submit"
+                                    class="btn btn-primary me-sm-3 me-1">Submit</button>
+                                <button type="reset"
+                                    class="btn btn-label-secondary">Cancel</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
