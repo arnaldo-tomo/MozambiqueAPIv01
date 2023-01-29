2112d797c121a0f93b7d54d833dc35e5ee0648b8
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-30 00:13:34 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Models\mocambique;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('v01//docs/mocambique', [mocambique::class, 'get']);
+Route::get('v01/docs/mocambique', [mocambique::class, 'get']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
