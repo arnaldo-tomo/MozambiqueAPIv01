6e9eaf833449b5cfca8faa9ed4041a7c8ec5b4b9
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-29 16:36:54 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\mocambique;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +20,24 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // php artisan db:seed
+        // php artisan db:seed --class=UserSeeder
+        // php artisan migrate:fresh --seed
+        // php artisan migrate:fresh --seed --seeder=UserSeeder
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'nome' => 'República de Moçambique',
+            'Presidente' =>  '@gmail.com',
+            'Primeiro-Ministro' =>  '@gmail.com',
+            'Sistema-Politico' =>  '@gmail.com',
+            'Data-da-Independencia' =>  '@gmail.com',
+            'Lingua-Oficial' =>  '@gmail.com',
+            'Capital' =>  '@gmail.com',
+            'Moeda' =>  '@gmail.com',
+            'Moeda' =>  '@gmail.com',
+            'GDP' =>  '@gmail.com',
+            'Codigo-Internet' =>  '@gmail.com',
+            'Codigo-Telefonico' =>  '@gmail.com',
+        ]);
     }
 }
