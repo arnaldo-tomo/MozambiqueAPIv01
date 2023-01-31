fabb8c84899b7277f22d32330831952cabe2dc12
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-31 13:58:52 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MocambiqueController;
 use App\Http\Controllers\Controller;
 use App\Http\Livewire\Login;
 use App\Http\Livewire\ManagerApi;
@@ -8,3 +9,5 @@ use Illuminate\Support\Facades\Route;
 route::get('/', [Controller::class, 'PaginaInicial']);
 route::get('cpainel', Login::class);
 route::get('managerapi', ManagerApi::class)->name('manegerapi');
+
+route::get('v01/docs/mocambique',[MocambiqueController::class,'get']);
