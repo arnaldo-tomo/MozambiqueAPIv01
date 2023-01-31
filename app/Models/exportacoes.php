8f438294599c5a90ce4e96a55ed7bc2480810c3b
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-31 13:58:15 +0200

    Create exportacoes.php

---
+++ app/Models/exportacoes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\mocambique;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class exportacoes extends Model
+{
+    use HasFactory;
+
+    public function mocambique()
+    {
+        return $this->belongsTo(mocambique::class);
+    }
+}
