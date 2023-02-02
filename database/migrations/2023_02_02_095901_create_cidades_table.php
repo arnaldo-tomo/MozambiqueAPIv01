379313b9a625ecbabeabb3c0f42c75b941ca11ba
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-02 13:03:55 +0200

    Create 2023_02_02_095901_create_cidades_table.php

---
+++ database/migrations/2023_02_02_095901_create_cidades_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cidades', function (Blueprint $table) {
+            $table->integer('CodigoCidade');
+            $table->string('Localidade');
+            $table->string('Provincia');
+            $table->integer('Populacao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cidades');
+    }
+};
