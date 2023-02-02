f0730f28e04df3ee3a11a591f10bf25a5f9b7ae2
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-02 11:26:37 +0200

    Update 2023_01_30_102437_create_inflaxaos_table.php

--- database/migrations/2023_01_30_102437_create_inflaxaos_table.php
+++ database/migrations/2023_01_30_102437_create_inflaxaos_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('inflaxaos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->string( 'Mes');
+            $table->string( 'Valor');
+            // $table->timestamps();
         });
     }
 
