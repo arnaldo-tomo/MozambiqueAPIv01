b6d5230b0c6c3d199b3ad29a8c8aa7fb4ff5bb51
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-02 11:35:30 +0200

    Update 2023_01_30_102018_create_provincias_table.php

--- database/migrations/2023_01_30_102018_create_provincias_table.php
+++ database/migrations/2023_01_30_102018_create_provincias_table.php
@@ -14,8 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('provincias', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->integer('CodigoCidade');
+            $table->string('Localidade');
+            $table->string('Provincia');
+            $table->integer('Populacao');
+            // $table->timestamps();
         });
     }
 
