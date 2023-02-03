cfc8beef6752001a54b375e4bf6a18638385e6ba
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-03 09:41:09 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 
-Route::get('v01/docs/mocambique', [MocambiqueController::class ,'get'] );
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('v01/docs/mocambique', [MocambiqueController::class, 'get']);
+Route::get('v01/docs/provincias', [Controller::class, 'get']);
+Route::get('v01/docs/provincia/{id}', [Controller::class, 'getters']);
